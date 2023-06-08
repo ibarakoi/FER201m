709a2fe471d2bf92551666d37424ea6d4a8031d5
ibarakoi <ddawng184@gmail.com>
2023-06-08 10:32:08 +0700

    ex7

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+export default class Navigation extends Component{
+    render(){
+        return(
+            <div className='nav'>
+                <nav>
+                    <ul className='nav-list'>
+                        <li className='nav-list--element'><a className='active' href ="#home">Home</a></li>
+                        <li className='nav-list--element'><a href ="#news">News</a></li>
+                        <li className='nav-list--element'><a href ="#about">About</a></li>
+                        <li className='nav-list--element'><a href ="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
