c3d4e2eee2ec0a121ba03740637e5a0fd7b73390
ibarakoi <ddawng184@gmail.com>
2023-06-08 10:33:59 +0700

    ex10

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default function Navigation() {
+  return (
+    <nav className='nav'>
+  <ul className='nav-list'>
+      <li className='nav-list--element'><a className='active' href='#home'>Home</a></li>
+      <li className='nav-list--element'><a href='#news'>News</a></li>
+      <li className='nav-list--element'><a href='#about'>About</a></li>
+      <li className='nav-list--element'><a href='#contact'>Contact</a></li>
+  </ul>
+</nav>
+  )
+}
