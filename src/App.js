c83049fba0b99e658f87da7c4a983ab278ecbcfb
ibarakoi <ddawng184@gmail.com>
2023-06-08 10:33:24 +0700

    ex9

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <div className='App'>
+      <Navigation/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+export default class player extends Component{
+    render(){
+        return(
+            <footer>
+                <p>copyright &copy; 2022</p>
+            </footer>
+        )
+    }
+}
